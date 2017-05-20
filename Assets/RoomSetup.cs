4d9c6970111e3db8ef0c4c8655d277c547795eb6
iidzuka <taki.hiyoshi@gmail.com>
2017-05-20 21:01:00 +0900

    update 接続したユーザーごとにカメラが割り当てられる

---
+++ Assets/RoomSetup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RoomSetup : NetworkBehaviour {
+	public GameObject tablePrefab;
+	public override void OnStartServer () {
+		var table = (GameObject)Instantiate(tablePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+		NetworkServer.Spawn(table);
+	}
+}
